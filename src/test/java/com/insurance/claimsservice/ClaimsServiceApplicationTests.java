a3c4ab4e88f9921eb1b226c21e4f2a37157a686a
ranjithaleti <ranjithaleti5@gmail.com>
2026-06-25 18:09:49 -0500

    Initial commit: Spring Boot project scaffold for claims-service

---
+++ src/test/java/com/insurance/claimsservice/ClaimsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.claimsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
